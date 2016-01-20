79b80f797b81f2172cafb2938e41ca552c1bc03f
Young <liyang_bsy@hotmail.com>
2016-01-20 15:08:00 +0800

    test

--- src/net/vicp/lylab/server/ServerRuntime.java
+++ src/net/vicp/lylab/server/ServerRuntime.java
@@ -3,11 +3,10 @@ package net.vicp.lylab.server;
 import java.io.File;
 
 import net.vicp.lylab.core.CoreDef;
-import net.vicp.lylab.utils.Config;
 
 public class ServerRuntime {
 	public static void main(String[] arg) {
-		CoreDef.config = new Config(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
+		CoreDef.config.reload(CoreDef.rootPath + File.separator + "config" + File.separator + "config.txt");
 	}
 	
 	public static void close() {
