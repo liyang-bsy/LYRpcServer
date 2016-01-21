37457a47d22cd1df994023db1822e4cd85eae33c
Young <liyang_bsy@hotmail.com>
2016-01-21 19:40:29 +0800

    update

--- src/net/vicp/lylab/server/action/rpc/RPCAction.java
+++ src/net/vicp/lylab/server/action/rpc/RPCAction.java
@@ -3,6 +3,8 @@ package net.vicp.lylab.server.action.rpc;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import net.vicp.lylab.core.CoreDef;
 import net.vicp.lylab.core.model.Message;
 import net.vicp.lylab.core.model.Pair;
@@ -22,38 +24,53 @@ public class RPCAction extends RPCBaseAction {
 
 	@Override
 	public void exec() {
-		RpcConnector connector = (RpcConnector) CoreDef.config.getConfig("Singleton").getObject("connector");
+		do {
+			// check server from request
+			if (StringUtils.isBlank(getRequest().getServer())) {
+				getResponse().setCode(0x00000101);
+				getResponse().setMessage("Server is blank");
+				break;
+			}
+			// check procedure from request
+			if (StringUtils.isBlank(getRequest().getKey())) {
+				getResponse().setCode(0x00000102);
+				getResponse().setMessage("Foreign key not found");
+				break;
+			}
 
-		RPCMessage req = getRequest();
-		List<Pair<String, Integer>> addrList = new ArrayList<>();
-		List<Pair<String, Message>> result = new ArrayList<>();
-		retry: while (true) {
-			try {
-				if (req.isBroadcast()) {
-					addrList = connector.getAllAddress(req.getServer());
-				} else {
-					addrList = connector.getOneRandomAddress(req.getServer());
-				}
-			} catch (Exception e) {
-				getResponse().fail("Access rpc server list faild:" + Utils.getStringFromException(e));
-				return;
-			}
-			for (Pair<String, Integer> addr : addrList) {
-				Message message = new Message();
+			RpcConnector connector = (RpcConnector) CoreDef.config.getConfig("Singleton").getObject("connector");
+
+			RPCMessage req = getRequest();
+			List<Pair<String, Integer>> addrList = new ArrayList<>();
+			List<Pair<String, Message>> result = new ArrayList<>();
+			retry: while (true) {
 				try {
-					message = connector.request(addr.getLeft(), addr.getRight(), req);
+					if (req.isBroadcast()) {
+						addrList = connector.getAllAddress(req.getServer());
+					} else {
+						addrList = connector.getOneRandomAddress(req.getServer());
+					}
 				} catch (Exception e) {
-					if (req.isBroadcast())
-						message.setMessage("Unreachable server:" + addr);
-					else
-						continue retry;
+					getResponse().fail("Access rpc server list faild:" + Utils.getStringFromException(e));
+					return;
 				}
-				result.add(new Pair<>(addr.getLeft(), message));
+				for (Pair<String, Integer> addr : addrList) {
+					Message message = new Message();
+					try {
+						message = connector.request(addr.getLeft(), addr.getRight(), req);
+					} catch (Exception e) {
+						if (req.isBroadcast())
+							message.setMessage("Unreachable server:" + addr);
+						else
+							continue retry;
+					}
+					result.add(new Pair<>(addr.getLeft(), message));
+				}
+				break;
 			}
-			break;
-		}
-		getResponse().getBody().put("BraodCastResult", result);
-		getResponse().success();
+			getResponse().getBody().put("BraodCastResult", result);
+			getResponse().success();
+		} while (false);
 	}
 
 }
--- src/net/vicp/lylab/server/rpc/RPCDispatcherAop.java
+++ src/net/vicp/lylab/server/rpc/RPCDispatcherAop.java
@@ -1,107 +1,30 @@
 package net.vicp.lylab.server.rpc;
 
-import java.net.Socket;
-
-import org.apache.commons.lang3.StringUtils;
-
-import net.vicp.lylab.core.BaseAction;
 import net.vicp.lylab.core.CoreDef;
 import net.vicp.lylab.core.interfaces.Aop;
 import net.vicp.lylab.core.model.Message;
+import net.vicp.lylab.core.model.RPCBaseAction;
 import net.vicp.lylab.core.model.RPCMessage;
 import net.vicp.lylab.server.aop.SimpleKeyDispatcherAop;
-import net.vicp.lylab.server.filter.Filter;
 import net.vicp.lylab.server.utils.Logger;
-import net.vicp.lylab.utils.Utils;
-import net.vicp.lylab.utils.internet.HeartBeat;
 
-public class RPCDispatcherAop extends SimpleKeyDispatcherAop implements Aop {
-	
+public class RPCDispatcherAop extends SimpleKeyDispatcherAop<RPCMessage> implements Aop {
 	@Override
-	public byte[] doAction(Socket client, byte[] requestByte, int offset) {
-		RPCMessage request = null;
+	protected void logger(RPCMessage request, Message response) {
+		System.out.println("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
+				+ "\nBefore:" + request + "\nAfter:" + response);
+		((Logger) CoreDef.config.getConfig("Singleton").getObject("Logger"))
+				.appendLine("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
+						+ "\nBefore:" + request + "\nAfter:" + response);
+	}
 
-		String key = null;
-		BaseAction action = null;
-		Message response = new Message();
+	@Override
+	protected RPCBaseAction mapAction(RPCMessage request) {
 		try {
-			do {
-				try {
-					Object obj = protocol.decode(requestByte, offset);
-					if(obj instanceof HeartBeat)
-						return protocol.encode(obj);
-					request = (RPCMessage) obj;
-				} catch (Exception e) {
-					log.debug(Utils.getStringFromException(e));
-				}
-				if(request == null) {
-					response.setCode(0x00000001);
-					response.setMessage("RPCMessage not found");
-					break;
-				}
-				// do start filter
-				if (filterChain != null && filterChain.size() != 0)
-					for (Filter filter : filterChain) {
-						Message ret = null;
-						if ((ret = filter.doFilter(client, request)) != null)
-							return protocol.encode(ret);
-					}
-				response.copyBasicInfo(request);
-				
-				// gain key from rpcReq
-				key = request.getRpcKey();
-				if (StringUtils.isBlank(key)) {
-					response.setCode(0x00000002);
-					response.setMessage("Rpc key not found");
-					break;
-				}
-				else if("RPC".equals(key)) {
-					// check server from request
-					if (StringUtils.isBlank(request.getServer())) {
-						response.setCode(0x00000101);
-						response.setMessage("Server is blank");
-						break;
-					}
-					// check procedure from request
-					if (StringUtils.isBlank(request.getKey())) {
-						response.setCode(0x00000102);
-						response.setMessage("Foreign key not found");
-						break;
-					}
-				}
-				// get action related to key
-				try {
-					action = (BaseAction) CoreDef.config.getConfig("Aop").getNewInstance(key + "Action");
-				} catch (Exception e) { }
-				if (action == null) {
-					response.setCode(0x00000003);
-					response.setMessage("Action not found");
-					break;
-				}
-				// Initialize action
-				action.setSocket(client);
-				action.setRequest(request);
-				action.setResponse(response);
-				// execute action
-				try {
-					action.exec();
-				} catch (Throwable t) {
-					String reason = Utils.getStringFromThrowable(t);
-					log.error(reason);
-					response.setCode(0x00000004);
-					response.setMessage("Action exec failed:" + reason);
-					break;
-				}
-			} while (false);
+			return (RPCBaseAction) CoreDef.config.getConfig("Aop").getNewInstance(request.getRpcKey() + "Action");
 		} catch (Exception e) {
-			log.error(Utils.getStringFromException(e));
+			return null;
 		}
-		// to logger
-		System.out.println("Access key:" + key + "\tAccess rpcKey:" + request.getRpcKey() + "\nBefore:" + request
-				+ "\nAfter:" + response);
-		((Logger) CoreDef.config.getConfig("Singleton").getObject("Logger")).appendLine("Access key:" + key
-				+ "\tAccess rpcKey:" + request.getRpcKey() + "\nBefore:" + request + "\nAfter:" + response);
-		return protocol.encode(response);
 	}
 
 }
