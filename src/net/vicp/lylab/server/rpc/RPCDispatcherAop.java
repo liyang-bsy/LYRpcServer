e3e4501413ee93d07aa503b844c04cddf24f3b1f
Young <liyang_bsy@hotmail.com>
2016-01-26 11:53:02 +0800

    milestone

--- src/net/vicp/lylab/server/rpc/RPCDispatcherAop.java
+++ src/net/vicp/lylab/server/rpc/RPCDispatcherAop.java
@@ -10,8 +10,8 @@ import net.vicp.lylab.server.utils.Logger;
 public class RPCDispatcherAop extends SimpleKeyDispatcher<RPCMessage> {
 	@Override
 	protected void logger(RPCMessage request, Message response) {
-		System.out.println("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
-				+ "\nBefore:" + request + "\nAfter:" + response);
+//		System.out.println("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
+//				+ "\nBefore:" + request + "\nAfter:" + response);
 		((Logger) CoreDef.config.getConfig("Singleton").getObject("Logger"))
 				.appendLine("Access key:" + request.getKey() + "\tAccess rpcKey:" + ((RPCMessage) request).getRpcKey()
 						+ "\nBefore:" + request + "\nAfter:" + response);
