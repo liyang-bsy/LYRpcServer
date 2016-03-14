1431a64776db11661a2bddf2560bc25052889cba
Young <liyang_bsy@hotmail.com>
2016-03-14 11:22:18 +0800

    update port

--- src/client/LYServerRequest.java
+++ src/client/LYServerRequest.java
@@ -9,16 +9,17 @@ import net.vicp.lylab.utils.internet.protocol.LYLabProtocol;
 public class LYServerRequest {
 
 	public static void main(String[] args) {
-		if (args.length <= 1) {
-			System.out.println("Addresses and commands?");
+		if (args.length <= 2) {
+			System.out.println("Addresses, port and commands?");
 			return;
 		}
 
 		String[] hosts = args[0].split("\\,");
-		String cmd = args[1];
+		String port = args[1];
+		String cmd = args[2];
 
 		for (String host : hosts) {
-			SyncSession session = new SyncSession(host, 2011, new LYLabProtocol(), new SimpleHeartBeat());
+			SyncSession session = new SyncSession(host, Integer.valueOf(port), new LYLabProtocol(), new SimpleHeartBeat());
 			session.initialize();
 
 			Message message = new Message();
